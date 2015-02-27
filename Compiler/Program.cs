1db21f4763b260b2869f38c2e16a981690841279
Andrew Heaford <AHPhoshi@gmail.com>
2015-02-27 11:51:50 +0000

    Parse and typecheck failures are now caught, printed, and exit the compiler with exit code 1. This is to aid with automatic testing of things that might fail.

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Speedycloud.Bytecode;
+using Speedycloud.Compiler.AST_Nodes;
+using Speedycloud.Compiler.Parser;
+using Speedycloud.Compiler.TypeChecker;
 
 namespace Speedycloud.Compiler {
     class Program {
-        static void Main(string[] args) {
+        static int Main(string[] args) {
             var code = File.ReadAllText(args[0]);
 
             Console.WriteLine("Input:");
@@ -18,10 +22,23 @@ namespace Speedycloud.Compiler {
             var parser = new Parser.Parser(lexer.Lex(code));
             var typechecker = new TypeChecker.Typechecker(prelude.Types());
             var compiler = new AST_Visitors.BytecodeGenerator(prelude.Definitions(), typechecker);
+            INode tree;
+            try {
+                tree = parser.ParseProgram();
+            }
+            catch (ParseException ex) {
+                Console.Write(ex);
+                return 1;
+            }
 
-            var tree = parser.ParseProgram();
+            try {
+                typechecker.Visit(tree);
+            }
+            catch (TypeCheckException ex) {
+                Console.Write(ex);
+                return 1;
+            }
 
-            typechecker.Visit(tree);
             var bytecode = compiler.Finalise(compiler.Visit(tree)).ToList();
 
             Console.WriteLine("Constants:");
@@ -43,8 +60,9 @@ namespace Speedycloud.Compiler {
             var bytecodeData = saver.Dump(bytecode, compiler.Constants.Values);
             //Console.WriteLine("Data:");
             //Console.Write(bytecodeData);
-            File.WriteAllText(args[1], bytecodeData);
-            Console.ReadKey();
+            var savename = args.Count() > 1 ? args[1] : args[0].Substring(0, args[0].IndexOf('.')) + ".elc";
+            File.WriteAllText(savename, bytecodeData);
+            return 0;
         }
     }
 }
